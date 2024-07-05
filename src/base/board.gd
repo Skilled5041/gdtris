06fe5a10ed93e573477df7382e544ba6fc7a6c47
Skilled5041 <skilled5041@gmail.com>
2024-07-05 16:09:58 -0400

    Added control functions and other stuff

--- src/base/board.gd
+++ src/base/board.gd
@@ -97,10 +97,12 @@ func hold():
     # Hold the piece
     if hold_piece == null:
         hold_piece = Piece.new(current_piece.type)
+        spawn_new_piece_from_bag()
     else:
-        var temp = hold_piece
-        hold_piece = Piece.new(current_piece.type)
-        current_piece = temp
+        var temp = Piece.new(current_piece.type)
+        current_piece = Piece.new(hold_piece.type)
+        hold_piece = Piece.new(temp.type)
+        spawn_new_piece(current_piece)
 
 func spawn_new_piece_from_bag():
     piece_queue.push_back(get_piece_from_bag())
@@ -114,4 +116,173 @@ func spawn_new_piece(piece: Piece):
     current_piece_top_left_corner = Vector2(3, 2)
 
     # Check if player is dead
-    for i in range(0, current_piece.tiles[0].)
+    for i in range(0, current_piece.tiles[0].size()):
+        for j in range(0, current_piece.tiles.size()):
+            if current_piece.tiles[j][i].state != Tile.State.EMPTY:
+                if board[i + 3][j + 2].state != Tile.State.EMPTY:
+                    alive = false
+                    game_ended = true
+                    return
+
+    # Merge the piece into the array
+    for i in range(current_piece.tiles[0].size()):
+        for j in range(current_piece.tiles.size()):
+            if current_piece.tiles[j][i].state != Tile.State.EMPTY:
+                board[i + 3][j + 2].state = current_piece.tiles[j][i].state
+                board[i + 3][j + 2].type = current_piece.tiles[j][i].type
+                current_piece_coordinates.push_back(Vector2(i + 3, j + 2))
+
+    ghost_coordinates = calculate_drop_position()
+    show_ghost(ghost_coordinates)
+
+func calculate_drop_position():
+    # Only include the lowest (highest) Y coordinate of each column
+    var filtered_coordinates: Array[Vector2] = []
+    var new_coordinates: Array[Vector2] = []
+
+    for point in current_piece_coordinates:
+        var add = true
+        for filtered_point in filtered_coordinates:
+            if point.x == filtered_point.x:
+                add = false
+                if point.y > filtered_point.y:
+                    filtered_point.y = point.y
+        if add:
+            filtered_coordinates.push_back(Vector2(point.x, point.y))
+
+    var amount_to_fall = 24
+    for point in filtered_coordinates:
+        # Check how far the piece can fall
+        for i in range(point.y + 1, 24):
+            if board[point.x][i].state == Tile.State.PLACED:
+                amount_to_fall = min(amount_to_fall, i - point.y - 1)
+                break
+            elif i == 23:
+                amount_to_fall = min(amount_to_fall, i - point.y)
+
+    for point in current_piece_coordinates:
+        if point.y + (0 if amount_to_fall == 25 else amount_to_fall) > 23:
+            return current_piece_coordinates
+        new_coordinates.push_back(Vector2(point.x, point.y + (0 if amount_to_fall == 24 else amount_to_fall)))
+
+    return new_coordinates
+
+func show_ghost(ghost_coords: Array[Vector2]):
+    if ghost_coords == null:
+        return
+    
+    for point in ghost_coords:
+        if point.y < 4:
+            continue
+        if board[point.x][point.y].state != Tile.State.EMPTY:
+            continue
+        board[point.x][point.y].type = Tile.TileType.GHOST
+
+func _init():
+    # Initialize the board and other variables
+    # Board is 10 x 24
+    for i in range(10):
+        board.append([])
+        for j in range(24):
+            board[i].push_back(Tile.new(Tile.TileType.EMPTY, Tile.State.EMPTY))
+
+    piece_queue = []
+    current_piece_coordinates = []
+    ghost_coordinates = []
+    highest_piece_row = 24
+
+    # Initialize the bags
+    bag_1 = []
+    bag_2 = []
+
+    # Add a pieces to bag 1 and shuffle
+    var random_values: Array[int] = []
+    for i in range(0, 7):
+        random_values.push_back(i)
+    random_values.shuffle()
+
+    for value in random_values:
+        bag_1.push_back(Piece.Pieces.values()[value])
+
+    # Add a pieces to bag 2 and shuffle
+    for i in range(0, 7):
+        random_values.push_back(i)
+    random_values.shuffle()
+
+    for value in random_values:
+        bag_2.push_back(Piece.Pieces.values()[value])
+
+    # Fill the queue with 5 pieces
+    for i in range(5):
+        piece_queue.push_back(get_piece_from_bag())
+
+func place_piece():
+    for point in current_piece_coordinates:
+        board[point.x][point.y].state = Tile.State.PLACED
+        highest_piece_row = min(highest_piece_row, point.y)
+
+    # Try clearing lines
+    var rows: Array[int] = clear_lines()
+
+    # Move the rows down
+    number_of_lines_cleared += rows.size()
+    var max_value = -1
+    if rows.size() > 0:
+        max_value = max(rows)
+
+    if max_value != -1:
+        move_rows_down(max_value, highest_piece_row, rows)
+
+    spawn_new_piece_from_bag()
+
+func clear_lines():
+    # Store all rows that might be full
+    var rows_to_check: Array[int] = []
+    for point in current_piece_coordinates:
+        if !rows_to_check.has(point.y):
+            rows_to_check.push_back(point.y)
+
+    var removed_rows: Array[int] = []
+
+    # Check if the rows are full
+    for row in rows_to_check:
+        var full = true
+        for i in range(10):
+            if board[i][row].state != Tile.State.PLACED:
+                full = false
+                break
+
+        if full:
+            removed_rows.push_back(row)
+            for i in range(10):
+                board[i][row].state = Tile.State.EMPTY
+                board[i][row].type = Tile.TileType.EMPTY
+
+    return removed_rows
+
+func move_rows_down(bottom: int, top: int, removed_rows: Array[int]):
+    var number_times_to_move_down = 0
+    for i in range(bottom, top - 1, -1):
+        if removed_rows.has(i):
+            number_times_to_move_down += 1
+            continue
+        else:
+            for j in range(10):
+                board[j][i + number_times_to_move_down].state = board[j][i].state
+                board[j][i + number_times_to_move_down].type = board[j][i].type
+                board[j][i].state = Tile.State.EMPTY
+                board[j][i].type = Tile.TileType.EMPTY
+
+    highest_piece_row += number_times_to_move_down
+
+func hard_drop():
+    for point in current_piece_coordinates:
+        board[point.x][point.y].state = Tile.State.EMPTY
+        board[point.x][point.y].type = Tile.TileType.EMPTY
+
+    current_piece_coordinates = ghost_coordinates
+    for point in current_piece_coordinates:
+        board[point.x][point.y].state = current_piece.tile_type
+        board[point.x][point.y].type = Tile.State.FALLING
+
+    place_piece()
